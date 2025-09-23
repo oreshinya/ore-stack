56cd1a886388e760f8b45b4efd895f6f5fbbe614
Shinya Takahashi <s.takahashi313@gmail.com>
2025-09-23 10:26:52 +0900

    Tinyfix

--- app/data/result.ts
+++ app/data/result.ts
@@ -1,9 +1,9 @@
 export type Result<T> =
-  | { success: true; val: T }
+  | { success: true; value: T }
   | { success: false; message: string };
 
-export function success<T>(val: T) {
-  return { success: true as const, val };
+export function success<T>(value: T) {
+  return { success: true as const, value };
 }
 
 export function failure(message: string) {
